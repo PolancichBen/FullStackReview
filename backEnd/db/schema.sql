8dbda3061c8db2ce4765f56b24770c30107acf9d
Benjamin Polancich <polancichben@gmail.com>
2020-06-13 17:21:55 -0500

    Schema Built

--- backEnd/db/schema.sql
+++ backEnd/db/schema.sql
@@ -1,16 +1,18 @@
 DROP DATABASE IF EXISTS gitHubRepoFetcher;
 
-CREATE gitHubRepoFetcher;
+CREATE DATABASE gitHubRepoFetcher;
 USE gitHubRepoFetcher;
 
 CREATE TABLE repos (
 id INT AUTO_INCREMENT PRIMARY KEY,
-gitId: INT,
-handle: VARCHAR(255) NOT NULL,
-repo_description: VARCHAR(255) NOT NULL,
-repo_url: VARCHAR(255) NOT NULL,
-owner_starred: VARCHAR(255) NOT NULL,
-owner_followers_url: VARCHAR(255) NOT NULL,
-owner_organizations_url: VARCHAR(255) NOT NULL
+gitId INT,
+creation DATE,
+handle VARCHAR(255) NOT NULL,
+repo_description VARCHAR(255) NOT NULL,
+repo_url VARCHAR(255) NOT NULL,
+owner_starred VARCHAR(255) NOT NULL,
+owner_starGazer INT,
+owner_followers_url VARCHAR(255) NOT NULL,
+owner_organizations_url VARCHAR(255) NOT NULL
 );
 
